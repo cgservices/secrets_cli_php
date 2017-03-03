0055d6ac129a25d742875c4e121eb3d8f7f7ecbe
Sander van Rossum <sander.van.rossum@cg.nl>
2017-03-03 09:06:27 +0100

    Init to defaults

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -12,9 +12,6 @@
     }
 
     public function execute() {
-      $key = \SecretsCli\Application::$secrets_key;
-      $file = \SecretsCli\Application::$secrets_file;
-
       if(file_exists(getcwd() .'/.secrets')) {
         $prompter = new \CLIFramework\Prompter();
         $answer = $prompter->ask('Already initialized, do you wish to reinitialize to defaults?',array('yes','no'));
@@ -24,7 +21,7 @@
       }
 
       if(is_writable(getcwd())) {
-        if(file_put_contents(getcwd() .'/.secrets', "# Vault 'storage_key' where your secrets will be kept\r\nSECRETS_STORAGE_KEY=$key\r\n\r\n# File where your secrets are kept\r\nSECRETS_FILE=$file\r\n") !== false) {
+        if(file_put_contents(getcwd() .'/.secrets', "# Vault 'storage_key' where your secrets will be kept\r\nSECRETS_STORAGE_KEY=secret/project/\r\n\r\n# File where your secrets are kept\r\nSECRETS_FILE=./.env\r\n") !== false) {
           $this->success('Created .secrets file successfully!');
         } else {
           $this->warning('Failed to write to file, check permissions');
