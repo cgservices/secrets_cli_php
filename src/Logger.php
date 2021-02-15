42793b10cad13a09a9143b328baa3723d9696932
dorumd <mardari.dorua@gmail.com>
2021-02-15 13:57:26 +0100

    Improved logging format

--- src/Logger.php
+++ src/Logger.php
@@ -8,6 +8,6 @@ class Logger extends AbstractLogger
 {
     public function log($level, $message, array $context = array())
     {
-        fwrite(STDOUT, sprintf('[%s] Message: %s Context: %s', $level, $message, json_encode($context)));
+        fwrite(STDOUT, sprintf('[%s] Message: %s Context: %s' . PHP_EOL, $level, $message, json_encode($context)));
     }
 }
